c7c1d324936505ca0b641f53cae6bfc09608ebe4
maria-guerrero <mariadol.gc@gmail.com>
2021-12-22 22:24:33 +0100

    add changes in html, css and js

--- js/main.js
+++ js/main.js
@@ -1,77 +1,111 @@
 'use strict';
 
-// NOS TRAEMOS LOS ELEMENTOS HTML
-const selectedElement = document.querySelector('.js_selected');
-const randomResult = document.querySelector('.js_random_result');
-const inputPlay = document.querySelector('.js_button');
 
-const playerResult = document.querySelector('.js_playerResult');
-const computerResult = document.querySelector('.js_computerResult');
+// TRAEMOS LOS ELEMENTOS HTML QUE VAMOS A UTILIZAR Y LES ASIGNAMOS UNA VARIABLE //
+const selectOption = document.querySelector('.js_select');
+const inputButton = document.querySelector('.js_input_button');
+const lestPlay = document.querySelector('.js_lets_play');
+const inputButtonReset = document.querySelector('.js_input_buttonReset');
+
+const playerScore = document.querySelector('.js_player_score');
+const pcScore = document.querySelector('.js_computer_score');
+
+let player = 0;
+let computer = 0;
 
 
-// DEFINIMOS LAS
+// CREAMOS LAS FUNCIONES //
 
 // Función que generará un número aleatorio
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    }
 
-function changeOptionsGame(resultVariable) {
+// Función que generará el resultado del usuario y del PC en función del valor
+function changeOptionsGame(computerValue) {
 
-    const valueEl = selectedElement.value;
+    // Creamos la variable del valor del usuario
+    const playerValue = selectOption.value;
 
-    if (valueEl === resultVariable) {
-        randomResult.innerHTML = 'Empate';
+    if(playerValue  === computerValue) {
+        lestPlay.innerHTML = 'Empate';
 
-        } else if (valueEl === 'piedra') {
+        } else if(playerValue === 'rock') {
 
-            if (resultVariable === 'tijera') {
-                randomResult.innerHTML = '¡Has ganado!';
-            } else if (resultVariable === 'papel') {
-                randomResult.innerHTML = '¡Has perdido!';
+            if(computerValue === 'scissors') {
+                lestPlay.innerHTML = '¡Has ganado!';
+                player++;
+            } else if(computerValue === 'paper') {
+                lestPlay.innerHTML = '¡Has perdido!';
+                computer++;
             }
 
-        } else if (valueEl === 'papel') {
+        } else if(playerValue === 'paper') {
 
-            if (resultVariable === 'papel') {
-                randomResult.innerHTML = 'Empate';
-            } else if (resultVariable === 'tijera'){
-                randomResult.innerHTML = '¡Has perdido!';
+            if(computerValue === 'scissors') {
+                lestPlay.innerHTML = '¡Has perdido!'
+                computer++;
+            } else if(computerValue === 'rock') {
+                lestPlay.innerHTML = '¡Has ganado!';
+                player++;
             }
 
-        } else if (valueEl === 'tijera') {
-            
-            if (resultVariable === 'piedra') {
-                randomResult.innerHTML = '¡Has ganado!';
-            } else if ( resultVariable ==='papel') {
-                randomResult.innerHTML = '¡Has ganado!';
+        } else if(playerValue === 'scissors') {
+
+            if(computerValue === 'rock') {
+                lestPlay.innerHTML = '¡Has perdido!';
+                computer++;
+            } else if(computerValue === 'paper') {
+                lestPlay.innerHTML = '¡Has ganado!';
+                player++;
             }
         }
-    
-    }
+    // Dentro de la función generamos el resultado de 'Jugador' y 'Ordenador'
+    playerScore.innerHTML = 'Jugador:  ' + player;
+    pcScore.innerHTML = 'Ordenador:  ' + computer;
+}
 
-function randomNumEl() {
-    // Generamos número aleatorio
+// Función donde decimos qué elegirá el ordenador según el número aleatorio
+function randomMove() {
+
     const randomNum = getRandomNumber(10);
 
     let computerSelection;
 
-    if (randomNum < 3) {
-    computerSelection = 'piedra';
-    } else if (randomNum >= 6) {
-        computerSelection = 'papel';
+   if(randomNum < 3) {
+    computerSelection = 'rock';
+   } else if(randomNum >= 6) {
+        computerSelection = 'paper';
+   } else {
+        computerSelection = 'scissors';
+   }
+
+   return computerSelection;
+}
+
+// Función para cuando el juego llegue a 10 movimientos, finalizar
+function tenMovements() {
+
+    let totalMovements = player + computer;
+
+    if(totalMovements <= 9) {
+        inputButtonReset.classList.add('hidden2');
+        inputButton.classList.add('buttonStyles');
     } else {
-        computerSelection = 'tijera';
+        inputButtonReset.classList.remove('hidden2');
+        inputButton.classList.add('hidden1');
     }
 
-    return computerSelection;
 }
 
+// Contenedor de funciones donde se ejecutarán todas
 function handleClickUpdate(event) {
     event.preventDefault();
-    let resultVariable = randomNumEl();
-    changeOptionsGame(resultVariable);
+    let computerValue = randomMove(); // <-- Creamos una variable que contenga la función randomMove(), y así poder usarla en la función changeOptionsGame.
+    changeOptionsGame(computerValue); // <-- A la función changeOptionsGame le asignamos el parámetro de la variable computerValue, el cual contiene la función randomMove.
+    tenMovements();
 }
 
-// CREAMOS EL EVENTO LISTENER
-inputPlay.addEventListener('click', handleClickUpdate);
+
+// CREAMOS EL EVENTO //
+inputButton.addEventListener('click', handleClickUpdate);
